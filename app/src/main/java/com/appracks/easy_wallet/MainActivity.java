fb33914dfc8c19e6e0481a55aaa98551a6c79919
osongae2 <osongae2@gmail.com>
2015-11-25 22:13:05 +0600

    customize nav menu

--- app/src/main/java/com/appracks/easy_wallet/MainActivity.java
+++ app/src/main/java/com/appracks/easy_wallet/MainActivity.java
@@ -3,7 +3,6 @@ package com.appracks.easy_wallet;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.res.Configuration;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +12,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -28,12 +26,72 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar=(Toolbar)findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
 
+        setSummery();
         setNavMenu();
-
+        setSummeryClick();
     }
 
+    private void setSummery(){}
+    private void setSummeryClick(){
+        LinearLayout ly_current_week_in,ly_current_week_ex,ly_current_month_in,ly_current_month_ex,ly_current_year_in,ly_current_year_ex,ly_total_in,ly_total_ex;
+        ly_current_week_in=(LinearLayout)findViewById(R.id.ly_current_week_in);
+        ly_current_week_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ly_current_week_ex=(LinearLayout)findViewById(R.id.ly_current_week_ex);
+        ly_current_week_ex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ly_current_month_in=(LinearLayout)findViewById(R.id.ly_current_month_in);
+        ly_current_month_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ly_current_month_ex=(LinearLayout)findViewById(R.id.ly_current_month_ex);
+        ly_current_month_ex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ly_current_year_in=(LinearLayout)findViewById(R.id.ly_current_year_in);
+        ly_current_year_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ly_current_year_ex=(LinearLayout)findViewById(R.id.ly_current_year_ex);
+        ly_current_year_ex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ly_total_in=(LinearLayout)findViewById(R.id.ly_total_in);
+        ly_total_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        ly_total_ex=(LinearLayout)findViewById(R.id.ly_total_ex);
+        ly_total_ex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
     private void setNavMenu(){
         myDrawer= (DrawerLayout) findViewById(R.id.myDrawer);
         actionBarDrawerToggle=new ActionBarDrawerToggle(this, myDrawer,toolbar, R.string.drawer_open, R.string.drawer_close);
@@ -44,12 +102,38 @@ public class MainActivity extends AppCompatActivity {
         ly_summery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Oh yes",Toast.LENGTH_LONG).show();
+                myDrawer.closeDrawer(GravityCompat.START);
+                Toast.makeText(getApplicationContext(), "You are here", Toast.LENGTH_LONG).show();
+            }
+        });
+        LinearLayout ly_income=(LinearLayout)findViewById(R.id.ly_income);
+        ly_income.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDrawer.closeDrawer(GravityCompat.START);
+                Toast.makeText(getApplicationContext(),"ly_income",Toast.LENGTH_LONG).show();
+            }
+        });
+        LinearLayout ly_expense=(LinearLayout)findViewById(R.id.ly_expense);
+        ly_expense.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDrawer.closeDrawer(GravityCompat.START);
+                Toast.makeText(getApplicationContext(),"ly_expense",Toast.LENGTH_LONG).show();
+            }
+        });
+        LinearLayout ly_graph=(LinearLayout)findViewById(R.id.ly_graph);
+        ly_graph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDrawer.closeDrawer(GravityCompat.START);
+                Toast.makeText(getApplicationContext(),"ly_graph",Toast.LENGTH_LONG).show();
             }
         });
     }
     @Override
     public void onBackPressed() {
+        myDrawer.closeDrawer(GravityCompat.START);
         AlertDialog.Builder alertDialog=new AlertDialog.Builder(this);
         alertDialog.setTitle("Are you sure to want to quit?");
         alertDialog.setCancelable(false);
